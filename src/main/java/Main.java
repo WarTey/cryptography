b7d8df34d142482d16824d3ef39cb3c848fb681b
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-02-21 20:37:43 +0100

    Ajout des vérifications des entrées et sorties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
-import java.util.Arrays;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
 public class Main {
 
     // Renvoie l'aide pour les arguments
-    public static String helpArguments () {
+    private static String helpArguments () {
         String space = "    ";
         // Exemple d'usage: filecrypt -enc|-dec -key K..K -in <input file> -out <output file>
         String errorMessage = "Example usage:\n";
@@ -14,9 +22,21 @@ public class Main {
         errorMessage += space + "-dec: decryption\n";
         errorMessage += space + "-key: secret key (48 characters)\n";
         errorMessage += space + "-in: input file\n";
-        errorMessage += space + "-out: output file\n";
+        errorMessage += space + "-out: output file (different from the input file)\n";
         // Renvoie du message d'aide
         return errorMessage;
+    }
+
+    private static void process(File fileInput, File fileOutput, String encryptionType, String key) throws IOException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
+        // Récupère les données du fichier
+        byte[] fileData = Files.readAllBytes(fileInput.toPath());
+        // Selon le mode choisi dans les arguments, lance le chiffrement ou déchiffrement
+        // Écrit de plus le résultat dans le fichier de sortie
+        if (encryptionType.equals("encryption"))
+            Files.write(fileOutput.toPath(), Cryptography.encrypt(fileData, key));
+        else if (encryptionType.equals("decryption"))
+            Files.write(fileOutput.toPath(), Cryptography.decrypt(fileData, key));
+        System.out.println("Fin d'exécution.");
     }
 
     public static void main(String[] args) throws Exception {
@@ -27,23 +47,33 @@ public class Main {
         String outputFile = Arguments.getFile(args, false);
 
         // Vérifie que les arguments existe
-        if (encryptionType != null && key != null && inputFile != null && outputFile != null) {
-            byte[] fileData = new byte[15];
-            for(int i = 0; i < fileData.length; i++)
-                fileData[i] = (byte) i;
-
-            System.out.println(Arrays.toString(fileData));
-            System.out.println(fileData.length);
-
-            byte[] eFileData = Cryptography.encrypt(fileData, "666666666666666666666666666666666666666666666666");
-
-            System.out.println(Arrays.toString(eFileData));
-            System.out.println(eFileData.length);
-
-            byte[] dFileData = Cryptography.decrypt(eFileData, "666666666666666666666666666666666666666666666666");
-
-            System.out.println(Arrays.toString(dFileData));
-            System.out.println(dFileData.length);
+        if (encryptionType != null && key != null && inputFile != null && outputFile != null && !inputFile.equals(outputFile)) {
+            // Initialise le fichier d'entrée et de sortie
+            File fileInput = new File(inputFile);
+            File fileOutput = new File(outputFile);
+            // Vérifie que le fichier d'entrée existe
+            if (fileInput.exists()) {
+                // Vérifie que le fichier de sortie n'existe pas
+                if (!fileOutput.exists())
+                    // Lance de processus de chiffrement/déchiffrement
+                    process(fileInput, fileOutput, encryptionType, key);
+                else {
+                    String answer;
+                    // Demande à l'utilisateur s'il est possible d'écraser le fichier de sortie
+                    do {
+                        System.out.println("Attention, le fichier de sortie existe déjà. Voulez-vous l'écraser? (Y/N)");
+                        // Récupère l'entrée de l'utilisateur
+                        answer = new Scanner(System.in).next();
+                    } while (!answer.equals("Y") && !answer.equals("N"));
+                    // Selon sa réponse lance le processus ou arrête de programme
+                    if (answer.equals("Y"))
+                        process(fileInput, fileOutput, encryptionType, key);
+                    else
+                        System.out.println("Processus interrompu.");
+                }
+            } else
+                // Affiche un message si le fichier d'entrée n'existe pas
+                System.out.println("Attention, le fichier d'entrée n'existe pas.");
         } else
             // Affiche un message d'aide si les arguments sont incorrects
             System.out.println(helpArguments());
