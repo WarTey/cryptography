b42c7879505ad1af96b911d60d1d50ebae663129
Valentin Magnan <valentin.magnan13008@gmail.com>
2020-02-24 21:01:57 +0100

    Création de l'exception FileIntegrityException lorsque le MAC n'est pas vérifié

---
+++ src/main/java/FileIntegrityException.java
@@ -0,0 +1,15 @@
+
+public class FileIntegrityException extends Exception {
+
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3604974696309735660L;
+
+	public FileIntegrityException() {
+		System.out.println("Fichier corrompu: Le MAC calculé ne correspond pas au MAC récupéré");
+	}
+
+}
