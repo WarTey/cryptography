a793bb606e241553a2d70e460a1b4372151f100a
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-02-25 22:56:50 +0100

    Ajout d'une fonction pour créer notre archive

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,5 +1,7 @@
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class FileManager {
 
@@ -16,5 +18,31 @@ public class FileManager {
         }
         // Les fichiers sont corrects
         return true;
+    }
+
+    // Crée une archive à partir des nouvelles données
+    public static void createArchive(ArrayList<String> inputFiles, ArrayList<byte[]> filesData, File fileOutput) throws IOException {
+        // Initialise le flux de sortie permettant d'écrire dans le fichier
+        FileOutputStream fileOutputStream = new FileOutputStream(fileOutput.getName());
+        // Initialise le flux de sortie permettant d'ajouter des données à l'archive
+        ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+        // Parcourt l'ensemble des fichiers d'entrées
+        for (int i = 0; i < inputFiles.size(); i++) {
+            // Ajoute le fichier au flux de sortie (l'archive)
+            zipOutputStream.putNextEntry(new ZipEntry(new File(inputFiles.get(i)).getName()));
+            // Initialise le flux d'entrée correspondant aux octets de notre fichier chiffré ou déchiffré
+            InputStream inputStream = new ByteArrayInputStream(filesData.get(i));
+            // Crée un buffer pour lire les octets du flux d'entrée
+            byte[] bytes = new byte[1024];
+            int length;
+            // Transfert les octets du flux d'entrée vers l'archive
+            while ((length = inputStream.read(bytes)) >= 0)
+                zipOutputStream.write(bytes, 0, length);
+            // Ferme le flux d'entrée
+            inputStream.close();
+        }
+        // Ferme les flux de sortie
+        zipOutputStream.close();
+        fileOutputStream.close();
     }
 }
