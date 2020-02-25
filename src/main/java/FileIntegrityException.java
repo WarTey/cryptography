fc262572e005aafb77bc32adbcbdfc8dc0c06d48
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-02-25 17:40:02 +0100

    Formatage du fichier

--- src/main/java/FileIntegrityException.java
+++ src/main/java/FileIntegrityException.java
@@ -1,15 +1,8 @@
-
 public class FileIntegrityException extends Exception {
 
-	
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -3604974696309735660L;
 
 	public FileIntegrityException() {
-		System.out.println("Fichier corrompu: Le MAC calculé ne correspond pas au MAC récupéré");
+		System.out.println("Fichier corrompu: Le MAC calculé ne correspond pas au MAC récupéré.");
 	}
-
 }
